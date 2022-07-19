03a960fe8a89fddc3eef31bfa33106585c7dc0e2
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2022-07-19 23:00:13 +0800

    ready for ssh servcer ell = 60

--- genes_entropy.sh
+++ genes_entropy.sh
@@ -9,7 +9,9 @@ PopulationSizeArray=("5" "10" "20")
 # PopulationSizeArray=("20" "40" "100")
 
 # PreviousSampledReferenceCountArr=("10" "5" "2" "1")
-PreviousSampledReferenceCountArr=("1" "2" "5" "10")
+# PreviousSampledReferenceCountArr=("1" "2" "5" "10")
+# PreviousSampledReferenceCountArr=("1" "10")
+PreviousSampledReferenceCountArr=("1")
 
 
 # ell
@@ -20,25 +22,25 @@ do
   for ((pop = 0; pop < 3; pop++))
   do
       # # repeat times
-      # for ((times = 0; times < 10; times++))
-      # do
-      #     echo "ell = ${ProblemSizeArray[$ell]}, pop_size = "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))", times = ${times}, Emax = "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))", PSRC = 1"
+      for ((times = 0; times < 10; times++))
+      do
+          echo "ell = ${ProblemSizeArray[$ell]}, pop_size = "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))", times = ${times}, Emax = "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))", PSRC = 1"
 
-      #     ./RankingEDAsCEC -i ./SDF_instance/"${ProblemSizeArray[$ell]}".txt\
-      #                       -o ./genes_entropy/result/"${ProblemSizeArray[$ell]}"_"$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"_1_AMR_"$times".txt\
-      #                       -s "$times"\
-      #                       -t SDF\
-      #                       -m AMR -d C -v 0\
-      #                       -p "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"\
-      #                       -e "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))"\
-      #                       -b 0.1\
-      #                       -r 1
-      # done
-      # # repeat times end
+          ./RankingEDAsCEC -i ./SDF_instance/"${ProblemSizeArray[$ell]}".txt\
+                            -o ./genes_entropy/result/"${ProblemSizeArray[$ell]}"_"$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"_1_AMR_"$times".txt\
+                            -s "$times"\
+                            -t SDF\
+                            -m AMR -d C -v 0\
+                            -p "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"\
+                            -e "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))"\
+                            -b 0.1\
+                            -r 1
+      done
+      # repeat times end
 
 
       # PSRC
-      for ((PSRC = 0; PSRC < 4; PSRC++))
+      for ((PSRC = 0; PSRC < 1; PSRC++))
       do
           # repeat times
           for ((times = 0; times < 10; times++))
