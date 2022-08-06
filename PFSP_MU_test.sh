6912d48668c4101b5f993a9751ef71d88246fd05
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2022-08-06 14:56:24 +0800

    mallow Ulam PFSP TSP SDF script

---
+++ PFSP_MU_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ProblemSizeArray=("10" "15" "20" "25" "30" "35" "40" "45" "50")
+# ProblemSizeArray=("6" "10" "20" "40" "60")
+# ProblemSizeArray=("20" "40" "60")
+ProblemSizeArray=("20" "50")
+Problem=("20_20" "50_20")
+# ProblemSizeArray=("60")
+# PopulationSizeArray=("2" "3" "5" "10" "20")
+PopulationSizeArray=("5" "10" "20")
+# PopulationSizeArray=("40" "20" "10" "5")
+# PopulationSizeArray=("20" "40" "100")
+
+# PreviousSampledReferenceCountArr=("10" "5" "2" "1")
+# PreviousSampledReferenceCountArr=("1" "2" "5" "10")
+# PreviousSampledReferenceCountArr=("1" "10")
+# PreviousSampledReferenceCountArr=("1")
+
+
+# ell
+for ((ell = 0; ell < 2; ell++))
+do
+
+  # Population Size
+  for ((pop = 0; pop < 3; pop++))
+  do
+      # # repeat times
+      for ((times = 0; times < 10; times++))
+      do
+          echo "problem = tai"${Problem[$ell]}".txt, pop_size = "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))", times = ${times}, Emax = "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))""
+
+          ./RankingEDAsCEC -i ./PFSP_instance/tai"${Problem[$ell]}".txt\
+                            -o ./PFSP_result/MU/"${ProblemSizeArray[$ell]}"_"$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"_MU_"$times".txt\
+                            -s "$times"\
+                            -t PFSP\
+                            -m M -d U -v 0\
+                            -p "$((${PopulationSizeArray[$pop]} * ${ProblemSizeArray[$ell]}))"\
+                            -e "$((${ProblemSizeArray[$ell]} * ${ProblemSizeArray[$ell]} * 1000))"
+      done
+      # repeat times end
+  done
+   # Population Size end
+done
+# ell end
